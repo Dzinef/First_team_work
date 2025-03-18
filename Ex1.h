6521a17a0b2a87c5b1654e92e728ca0845b36b65
Dzinef <andorelov@gmail.com>
2025-03-18 10:58:10 +0300

    Update Ex1.h

--- Ex1.h
+++ Ex1.h
@@ -2,13 +2,13 @@
 #ifndef Ex1_H
 #define Ex1_H
 
-#include<iostream>
+#include <iostream>
 
 bool CheckSize(int32_t);
 template <typename T>
 void PrintArray(const T* arr, int32_t size)
 {
-    for (int32_t i = 0; i < size; i++)
+    for (int32_t i{0}; i < size; ++i)
     {
         std::cout << arr[i] << " ";
     }
